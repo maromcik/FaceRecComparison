599f026709c3ec0cd752c817f7dc95f5f2baab0d
roman <roman.mariancik@gmail.com>
2022-10-15 18:48:35 +0200

    initial

--- tester.py
+++ tester.py
@@ -6,6 +6,7 @@ import random
 from retinaface import RetinaFace
 from mtcnn import MTCNN
 from face_detector import YoloDetector
+from ultraface import UltraFace
 
 
 class DetectorTester:
@@ -17,11 +18,14 @@ class DetectorTester:
         self.dlib_detector = dlib.get_frontal_face_detector()
         self.mtcnn_detector = MTCNN()
         self.yolo_detector = YoloDetector(target_size=640, gpu=-1, min_face=90)
+        self.ultra_face_detector = UltraFace()
+
         self.detectors = {'cv2': self.cv_detect,
                           'dlib-hog': self.dlib_detect,
                           'retina': self.retina_detect,
                           'mtcnn': self.mtcnn_detect,
-                          'yolo': self.yolo_detect}
+                          'yolo': self.yolo_detect,
+                          'ultraface': self.ultra_face_detect}
 
     def resize_img(self, img, x=640, y=480):
         return cv2.resize(img, (x, y), interpolation=cv2.INTER_NEAREST)
@@ -60,6 +64,9 @@ class DetectorTester:
         bbox, points = self.yolo_detector.predict(img)
         return bbox[0]
 
+    def ultra_face_detect(self, img):
+        return self.ultra_face_detector.detect(img)
+
     def detect(self, img, detector):
         start = time.time()
         faces = self.detectors[detector](img)
@@ -82,7 +89,7 @@ class DetectorTester:
             img = self.resize_img(img)
             faces, t = self.detect(img, detector)
             for (x, y, w, h) in faces:
-                if detector in ['yolo', 'retina-face']:
+                if detector in ['yolo', 'retina-face', 'ultraface']:
                     cv2.rectangle(img, (x, y), (w, h), (0, 255, 0), 2)
                 else:
                     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
@@ -94,14 +101,14 @@ class DetectorTester:
             if k == 27:
                 break
 
-    def test_on_pictures(self):
+    def test_on_pictures(self, detector):
         total_t = 0
         for image in self.prepare_pictures(self.folders[0]):
-            faces, t = self.detect(image, 'retina')
+            faces, t = self.detect(image, detector)
             print(t)
             total_t += t
         print("avg:", total_t / 10)
 
 
 dt = DetectorTester()
-dt.test_on_video(0, 'yolo')
+dt.test_on_video(0, 'ultraface')
---
+++ ultraface.py
@@ -0,0 +1,75 @@
+"""
+This code uses the onnx model to detect faces from live video or cameras.
+"""
+import time
+
+import cv2
+import numpy as np
+import onnx
+import vision.utils.box_utils_numpy as box_utils
+from caffe2.python.onnx import backend
+
+# onnx runtime
+import onnxruntime as ort
+
+
+class UltraFace:
+
+    def __init__(self, width=320, height=240):
+        label_path = "models/voc-model-labels.txt"
+        self.width = width
+        self.height = height
+        self.onnx_path = f"models/onnx/version-RFB-{self.width}.onnx"
+
+        self.class_names = [name.strip() for name in open(label_path).readlines()]
+
+        self.predictor = onnx.load(self.onnx_path)
+        onnx.checker.check_model(self.predictor)
+        onnx.helper.printable_graph(self.predictor.graph)
+        self.predictor = backend.prepare(self.predictor, device="CPU")  # default CPU
+
+        self.ort_session = ort.InferenceSession(self.onnx_path)
+        self.input_name = self.ort_session.get_inputs()[0].name
+        self.threshold = 0.7
+        self.sum = 0
+
+    def predict(self, width, height, confidences, boxes, prob_threshold, iou_threshold=0.3, top_k=-1):
+        boxes = boxes[0]
+        confidences = confidences[0]
+        picked_box_probs = []
+        picked_labels = []
+        for class_index in range(1, confidences.shape[1]):
+            probs = confidences[:, class_index]
+            mask = probs > prob_threshold
+            probs = probs[mask]
+            if probs.shape[0] == 0:
+                continue
+            subset_boxes = boxes[mask, :]
+            box_probs = np.concatenate([subset_boxes, probs.reshape(-1, 1)], axis=1)
+            box_probs = box_utils.hard_nms(box_probs,
+                                           iou_threshold=iou_threshold,
+                                           top_k=top_k,
+                                           )
+            picked_box_probs.append(box_probs)
+            picked_labels.extend([class_index] * box_probs.shape[0])
+        if not picked_box_probs:
+            return np.array([]), np.array([]), np.array([])
+        picked_box_probs = np.concatenate(picked_box_probs)
+        picked_box_probs[:, 0] *= width
+        picked_box_probs[:, 1] *= height
+        picked_box_probs[:, 2] *= width
+        picked_box_probs[:, 3] *= height
+        return picked_box_probs[:, :4].astype(np.int32), np.array(picked_labels), picked_box_probs[:, 4]
+
+    def detect(self, img):
+        image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, (self.width, self.height))
+        image_mean = np.array([127, 127, 127])
+        image = (image - image_mean) / 128
+        image = np.transpose(image, [2, 0, 1])
+        image = np.expand_dims(image, axis=0)
+        image = image.astype(np.float32)
+        confidences, boxes = self.ort_session.run(None, {self.input_name: image})
+        boxes, labels, probs = self.predict(img.shape[1], img.shape[0], confidences, boxes, self.threshold)
+        return boxes
+
